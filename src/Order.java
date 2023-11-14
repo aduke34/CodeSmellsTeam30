af7634b3e3f755e97ed91dba2797f726ff428a19
aduke34 <aduke34@gatech.edu>
2023-11-14 17:05:44 -0500

    everything carina made

---
+++ src/Order.java
@@ -0,0 +1,117 @@
+import java.util.List;
+
+public class Order {
+    private List<Item> items;
+    private String customerName;
+    private String customerEmail;
+
+    public Order(List<Item> items, String customerName, String customerEmail) {
+        this.items = items;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+    }
+
+    public double calculateTotalPrice() {
+    	double total = 0.0;
+    	for (Item item : items) {
+        	double price = item.getPrice();
+        	switch (item.getDiscountType()) {
+            	case PERCENTAGE:
+                	price -= item.getDiscountAmount() * price;
+                	break;
+            	case AMOUNT:
+                	price -= item.getDiscountAmount();
+                	break;
+            	default:
+                	// no discount
+                	break;
+        	}
+        	total += price * item.getQuantity();
+       	    total += isTaxableItem(item);
+        }
+    	if (hasGiftCard()) {
+        	total -= 10.0; // subtract $10 for gift card
+    	}
+
+        // apply 10% discount for orders over $100
+    	return total > 100.0 ? total *= 0.9 : total;
+    }
+
+    public double isTaxableItem(Item item) {
+        if (item instanceof TaxableItem) {
+            TaxableItem taxableItem = (TaxableItem) item;
+            double tax = taxableItem.getTaxRate() / 100.0 * item.getPrice();
+            return tax;
+        }
+        return 0.0;
+    }
+
+    public void sendConfirmationEmail() {
+        String message = "Thank you for your order, " + customerName + "!\n\n" +
+                "Your order details:\n";
+        for (Item item : items) {
+            message += item.getName() + " - " + item.getPrice() + "\n";
+        }
+        message += "Total: " + calculateTotalPrice();
+        EmailSender.sendEmail(customerEmail, "Order Confirmation", message);
+    }
+
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean hasGiftCard() {
+        boolean has_gift_card = false;
+        for (Item item : items) {
+            if (item instanceof GiftCardItem) {
+                has_gift_card = true;
+                break;
+            }
+        }
+        return has_gift_card;
+    }
+
+   public void printOrder() {
+        System.out.println("Order Details:");
+        for (Item item : items) {
+            System.out.println(item.getName() + " - " + item.getPrice());
+        }
+   }
+
+   public void addItemsFromAnotherOrder(Order otherOrder) {
+        for (Item item : otherOrder.getItems()) {
+            items.add(item);
+        }
+   }
+
+}
+
